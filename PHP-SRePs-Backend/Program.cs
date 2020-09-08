e685241ac3476f887d821f86bbab8b578eaa9562
phillipweinstock <phillipweinstock@users.noreply.github.com>
2020-09-08 18:38:47 +1000

    Got experimental Serverside RPC example implementation Adam take a look

--- PHP-SRePs-Backend/Program.cs
+++ PHP-SRePs-Backend/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
+//Note this might need to be deleted as a gRPC server project may 
+//Need to be created
+//TODO
+
 
 namespace PHP_SRePs_Backend
+
 {
     class Program
     {
-        static void Main(string[] args)
+        const int Port = 4121;
+        static void  Main(string[] args)
         {
             Console.WriteLine("Hello World lets do this :D!");
-            Console.ReadLine();
+            Server server = new Server
+            {
+                Services = { HelloService.BindService(new HelloTask()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            server.ShutdownAsync().Wait();
         }
+ 
     }
 }
---
+++ PHP-SRePs-Backend/Services/HelloTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Core;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace PHP_SRePs_Backend
+{
+    class HelloTask : HelloService.HelloServiceBase
+    {
+        public override Task<HelloReply> Hello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Name = "Hello" + request.Name });
+        }
+    }
+}
