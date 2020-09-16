272919c0435922cc864f7ba03711d2d9c98cae56
phillipweinstock <phillipweinstock@users.noreply.github.com>
2020-09-16 22:35:29 +1000

    tweaks to script

--- Database Resources/Setup_windows.ps1
+++ Database Resources/Setup_windows.ps1
@@ -1,4 +1,10 @@
 $url="https://www.apachefriends.org/xampp-files/7.2.33/xampp-windows-x64-7.2.33-0-VC15-installer.exe"
+#self elevates to administrator, 
+#why? because who wants to keep on clicking run as admin
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+#kudos 
+#https://stackoverflow.com/questions/7690994/running-a-command-as-administrator-using-powershell
+# 
 Write-Host "Congratulations on installing the PHP-SReps software"
 Write-Host "Please wait as we download the required software"
 Write-Host "This Program should automatically close after it finishs"
@@ -11,18 +17,21 @@ Write-Host "If this is the case, please manually spin up the Database"
 Read-Host -Prompt 'Press Enter to Continue'
 Write-Host "WARNING DO NOT PRESS ENTER TILL NAME ENTERED OR YOU INSTALLATION WILL BE BROKEN"
 $name_db = Read-Host -Prompt 'What would you like the DB to be called'
+Write-Host "WARNING DO NOT PRESS ENTER TILL PASSWORD ENTERED OR YOU INSTALLATION WILL BE BROKEN"
+$Password = Read-Host -Prompt 'What would you like the root password to be'
 #Download the package
 $runpath = "$PSScriptRoot\xamp.exe"
 $sqlfilePath = "$PSScriptRoot\sales_maybe.sql"
 $db_builder = Get-Content $sqlfilePath -Raw
 Write-Host "Please wait as we download the required software"
 
-Invoke-WebRequest -Uri $url -OutFile $runpath
+#Invoke-WebRequest -Uri $url -OutFile $runpath
 
 Write-Host "Currently installing please wait"
 Write-Host "This may take some time, after the installation is complete the DB will be spun up"
 #run the installation 
 Start-process -wait -FilePath $runpath -ArgumentList "--installer-language en --mode unattended --disable-components xampp_filezilla,xampp_mercury,xampp_tomcat,xampp_perl,xampp_phpmyadmin,xampp_webalizer,xampp_sendmail" -Verb RunAs
+
 
 
 
@@ -33,8 +42,7 @@ Start-Sleep -s 5
 Invoke-Expression "& `C:\xampp\mysql\bin\mysql.exe` -u root -e `"create database $name_db;`""
 #magic demonic stuff lmao
 Invoke-Expression "& `C:\xampp\mysql\bin\mysql.exe` -u root $name_db -e `"$db_builder`""
-Write-Host "WARNING DO NOT PRESS ENTER TILL PASSWORD ENTERED OR YOU INSTALLATION WILL BE BROKEN"
-$Password = Read-Host -Prompt 'What would you like the root password to be'
+
 #dont press enter or else lmao
 Invoke-Expression "& `C:\xampp\mysql\bin\mysqladmin.exe` -u root password `"$Password`" "
 #Stop MYSQL Server
