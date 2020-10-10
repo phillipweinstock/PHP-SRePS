5f120cc472992fb26462032fb6e18868a694ef2f
Adam Surgeon <54125546+adamsurge@users.noreply.github.com>
2020-10-10 16:30:28 +1100

    Added GetAllStocks - needs testing

--- PHP-SRePs-Backend/Services/StockService.cs
+++ PHP-SRePs-Backend/Services/StockService.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Core;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
 using MySqlConnector;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,19 @@ using System.Threading.Tasks;
 namespace PHP_SRePS_Backend
 {
     public class StockService : StockDef.StockDefBase
-{
+    {
+
+        private readonly ILogger<StockService> _logger;
+        public StockService(ILogger<StockService> logger)
+        {
+            _logger = logger;
+        }
+
         private AppDb db = new AppDb();
 
+        private ItemService itemService;
 
-/*        public override async Task<StockTake> GetAllStocks(HasChanged request,IServerStreamWriter<StockTake> responseStream, ServerCallContext context)
+        public override async Task GetAllStocks(HasChanged request, IServerStreamWriter<StockTake> responseStream, ServerCallContext context)
 
         {
             string query = "SELECT * FROM STOCKS;";
@@ -23,18 +32,36 @@ namespace PHP_SRePS_Backend
             var command = new MySqlCommand(query, db.Connection);
             var reader = await command.ExecuteReaderAsync();
 
+
+
             while (await reader.ReadAsync())
             {
-                var Stocktake = new StockTake();
+                // Use item service to get the item
+                Item item = await itemService.GetItem(new ItemGet()
                 {
-                    StockTake. = (uint)(int)reader.GetValueAs(0);
+                    ItemId = reader.GetFieldValue<uint>(1)
+                }, context);
+
+                // Enter info into StockTake message
+                var Stocktake = new StockTake
+                {
+                    Item = item,
+                    Info = new StockInfo
+                    {
+                        Stock = reader.GetFieldValue<int>(2)
+                    }
                 };
-                await responseStream.WriteAsync();
-        }*/
+
+                // send stock take over stream
+                await responseStream.WriteAsync(Stocktake);
+            }            
+        }
+
+
         public override async Task<StockInfo> GetStock(Item request, ServerCallContext context)
         {
 
-            
+
             await db.Connection.OpenAsync();
 
             var cmd = db.Connection.CreateCommand();
@@ -49,20 +76,13 @@ namespace PHP_SRePS_Backend
             await Stock.ReadAsync();
 
 
-            Stockinfo.Stock =  Stock.GetFieldValue<int>(2);
-
-                
-            
+            Stockinfo.Stock = Stock.GetFieldValue<int>(2);
 
             await Stock.CloseAsync();
             await Stock.DisposeAsync();
 
-          
+
             return (Stockinfo);
         }
-
     }
-
-
-
 }
