f25bf2963636a4864fe120f628b700961f929009
Adam Surgeon <54125546+adamsurge@users.noreply.github.com>
2020-10-13 15:44:14 +1100

    Implement GetMonthlyReportAsCSV Service

--- PHP-SRePs-Backend/Services/ReportService.cs
+++ PHP-SRePs-Backend/Services/ReportService.cs
@@ -1,6 +1,7 @@
 ﻿using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using MySqlConnector;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PHP_SRePS_Backend
@@ -14,9 +15,9 @@ namespace PHP_SRePS_Backend
             _logger = logger;
         }
 
-        public override async Task GetMontlyReport(DateGet request, IServerStreamWriter<MonthlyItemInfo> responseStream, ServerCallContext context)
+        public override async Task GetMonthlyReport(DateGet request, IServerStreamWriter<MonthlyItemInfo> responseStream, ServerCallContext context)
         {
-            _logger.LogInformation("IN");
+            _logger.LogInformation($"Requested month: {request.Month}, year: {request.Year}");
 
 
             MySqlConnection db = new AppDb().Connection;
@@ -49,5 +50,37 @@ namespace PHP_SRePS_Backend
             await db.CloseAsync();
             await db.DisposeAsync();
         }
-    }
+
+        public override async Task<MonthlyCSV> GetMonthlyReportAsCSV(DateGet request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Requested CSV for month: {request.Month}, year: {request.Year}");
+
+            MySqlConnection db = new AppDb().Connection;
+            await db.OpenAsync();
+
+            var cmd = db.CreateCommand();
+
+            cmd.CommandText = "SELECT ITEM.item_id, ITEM.name, SUM(ITEMDETAIL.quantity), SUM(ITEMDETAIL.quantity)* ITEM.price" +
+                              " FROM ITEMDETAIL" +
+                              " JOIN ITEM ON ITEMDETAIL.item_id = ITEM.item_id" +
+                              " JOIN SALE ON sale.sale_id = ITEMDETAIL.sale_id" +
+                              $" WHERE month(date)={request.Month} AND year(date)={request.Year}" +
+                              " GROUP BY ITEM.item_id" +
+                              " ORDER BY SALE.sale_id;";
+
+            var reader = await cmd.ExecuteReaderAsync();
+
+            List<string> csvRows = new List<string>();
+
+            while (await reader.ReadAsync())
+            {
+                csvRows.Add($"{reader.GetFieldValue<uint>(0)}, {reader.GetFieldValue<string>(1)}, {reader.GetFieldValue<float>(2)}, {reader.GetFieldValue<float>(3)}");
+            }
+
+            return (new MonthlyCSV
+            {
+                CsvRow = { csvRows }
+            });
+        }
+    }    
 }
