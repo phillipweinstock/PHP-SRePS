3c5812db45dd96d2beb603ce5fe1bf914181ec63
Adam Surgeon <54125546+adamsurge@users.noreply.github.com>
2020-10-21 23:13:16 +1100

    Added Prediction Form

--- PHP-SRePs-Frontend/PredictItem.Designer.cs
+++ PHP-SRePs-Frontend/PredictItem.Designer.cs
@@ -29,11 +29,183 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.predictGraph = new ScottPlot.FormsPlot();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtItemID = new System.Windows.Forms.TextBox();
+            this.txtDaysAhead = new System.Windows.Forms.TextBox();
+            this.ys = new System.Windows.Forms.Label();
+            this.btnPredict = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblVal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // predictGraph
+            // 
+            this.predictGraph.Location = new System.Drawing.Point(177, 12);
+            this.predictGraph.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.predictGraph.Name = "predictGraph";
+            this.predictGraph.Size = new System.Drawing.Size(610, 426);
+            this.predictGraph.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter month(1-12)";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGo.Location = new System.Drawing.Point(14, 239);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(138, 38);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMonth.Location = new System.Drawing.Point(13, 37);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(139, 33);
+            this.txtMonth.TabIndex = 3;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtYear.Location = new System.Drawing.Point(13, 104);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(139, 33);
+            this.txtYear.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(13, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Item ID";
+            // 
+            // txtItemID
+            // 
+            this.txtItemID.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtItemID.Location = new System.Drawing.Point(14, 178);
+            this.txtItemID.Name = "txtItemID";
+            this.txtItemID.Size = new System.Drawing.Size(139, 33);
+            this.txtItemID.TabIndex = 3;
+            // 
+            // txtDaysAhead
+            // 
+            this.txtDaysAhead.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDaysAhead.Location = new System.Drawing.Point(12, 315);
+            this.txtDaysAhead.Name = "txtDaysAhead";
+            this.txtDaysAhead.Size = new System.Drawing.Size(139, 33);
+            this.txtDaysAhead.TabIndex = 3;
+            // 
+            // ys
+            // 
+            this.ys.AutoSize = true;
+            this.ys.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ys.Location = new System.Drawing.Point(12, 291);
+            this.ys.Name = "ys";
+            this.ys.Size = new System.Drawing.Size(92, 21);
+            this.ys.TabIndex = 1;
+            this.ys.Text = "Days Ahead";
+            // 
+            // btnPredict
+            // 
+            this.btnPredict.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPredict.Location = new System.Drawing.Point(12, 354);
+            this.btnPredict.Name = "btnPredict";
+            this.btnPredict.Size = new System.Drawing.Size(138, 38);
+            this.btnPredict.TabIndex = 2;
+            this.btnPredict.Text = "Predict";
+            this.btnPredict.UseVisualStyleBackColor = true;
+            this.btnPredict.Click += new System.EventHandler(this.btnPredict_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Prediction:";
+            // 
+            // lblVal
+            // 
+            this.lblVal.AutoSize = true;
+            this.lblVal.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblVal.Location = new System.Drawing.Point(129, 408);
+            this.lblVal.Name = "lblVal";
+            this.lblVal.Size = new System.Drawing.Size(0, 30);
+            this.lblVal.TabIndex = 5;
+            // 
+            // PredictItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblVal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPredict);
+            this.Controls.Add(this.ys);
+            this.Controls.Add(this.txtDaysAhead);
+            this.Controls.Add(this.txtItemID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.predictGraph);
+            this.Name = "PredictItem";
             this.Text = "PredictItem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ScottPlot.FormsPlot predictGraph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button b;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtItemID;
+        private System.Windows.Forms.TextBox Yearh;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtDaysAhead;
+        private System.Windows.Forms.Label ys;
+        private System.Windows.Forms.Button btnPredict;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblVal;
     }
 }
--- PHP-SRePs-Frontend/PredictItem.cs
+++ PHP-SRePs-Frontend/PredictItem.cs
@@ -1,18 +1,96 @@
-﻿using System;
+﻿using PHP_SRePS_Backend;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Grpc.Core;
+using System.Linq;
+using System.Runtime.InteropServices;
+using ScottPlot.Statistics;
+using ScottPlot;
 
 namespace PHP_SRePS_Frontend
 {
     public partial class PredictItem : Form
     {
-        public PredictItem()
+        LinearRegressionLine model;
+        List<double> dataX;
+        List<double> dataY;
+
+        public PredictItem(ReportMenu form)
         {
             InitializeComponent();
+            _ = Gprc_channel_instance.GetInstance();
+
+            dataX = new List<double>();
+            dataY = new List<double>();
+
+        }
+
+        private async void btnGo_Click(object sender, EventArgs e)
+        {
+            var client = Gprc_channel_instance.ReportClient;
+
+            var input = new LinearGet
+            {
+                DateInfo = new DateGet
+                {
+                    Month = int.Parse(txtMonth.Text.Trim()),
+                    Year = int.Parse(txtYear.Text.Trim())
+                },
+                ItemId = uint.Parse(txtItemID.Text.Trim())
+            };
+
+            var reply = await client.GetPredictionReportAsync(input);
+
+            dataY.AddRange(reply.Sales);
+
+            for (int i = 0; i < dataY.Count; i++)
+            {
+                dataX.Add(i);
+            }
+
+            List<double> Sales_list = new List<double>();
+
+
+            Console.WriteLine(dataY.Count);
+            Console.WriteLine(dataX.Count);
+
+            this.model = new ScottPlot.Statistics.LinearRegressionLine(dataX.ToArray(), dataY.ToArray());
+            var slope = model.slope;
+
+            var offset = model.offset;
+            var pearsonsquared = model.rSquared;
+
+            predictGraph.plt.Title($"Y = {model.slope:0.0000}x + {model.offset:0.0}\nR² = {model.rSquared:0.0000}");
+            predictGraph.plt.XLabel("Days");
+            predictGraph.plt.YLabel("Sales");
+            predictGraph.plt.PlotScatter(dataX.ToArray(), dataY.ToArray());
+
+            predictGraph.plt.PlotLine(model.slope, model.offset, (dataX[0], dataX[dataX.Count - 1]), lineWidth: 2);
+            predictGraph.plt.AxisAuto();
+            predictGraph.Render();
+
+        }
+
+        private void btnPredict_Click(object sender, EventArgs e)
+        {
+            lblVal.Text = $"{double.Parse($"{model.slope:0.0000}") * double.Parse(txtDaysAhead.Text.Trim()) + double.Parse($"{model.offset}"):0.0000}";
+
+            var days = int.Parse(txtDaysAhead.Text.Trim());
+
+            List<double> dataX2 = new List<double>();
+            dataX2.Add(dataX[dataX.Count - 1]);
+            dataX2.Add(dataX[dataX.Count - 1] + days);
+            List<double> dataY2 = new List<double>();
+            dataY2.Add(dataY[dataY.Count - 1]);
+            dataY2.Add(((dataX[dataX.Count - 1] + days) * model.slope) + model.offset);
+            predictGraph.plt.PlotScatter(dataX2.ToArray(), dataY2.ToArray(), markerSize: 7, markerShape: MarkerShape.filledSquare);
+            predictGraph.plt.AxisAuto();
+            predictGraph.Render();
         }
     }
 }
